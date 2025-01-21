3227fb0097d80fb4ddc9dcde919e54abc3d6a2a1
Abdu201213 <sabdu9049@gamil.com>
2025-01-21 23:36:13 +0300

    test for the styled componets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 import './App.css'
 import Landing from './pages/Landing'
+const Button = styled.button`
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;   
 
+  `
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Landing/>
+      <Button>Click me</Button>
     </>
   )
 }
