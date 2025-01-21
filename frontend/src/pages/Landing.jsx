166270fedb2194841d02efebe46362123a49554b
Abdu201213 <sabdu9049@gamil.com>
2025-01-21 23:36:52 +0300

    adding the paragraph and login or register buttton

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div>
-      landing
-    </div>
+    <main>
+      <nav>
+         <h1>Jobify</h1>
+      </nav>
+      <div className="container page-page">
+         <div className="page-info">
+            <div className="img">jobify</div>
+            <h1>Job Tracking Application</h1>
+            <p>
+               I'm baby street art shoreditch try-hard taiyaki 
+               pitchfork +1 master cleanse 90's letterpress health 
+               goth sustainable. JOMO mukbang cronut, 90's lumbersexual 
+            </p>
+            <button>Login/Register</button>
+         </div>
+         <div>img</div>
+      </div>
+    </main>
   )
 }
 
