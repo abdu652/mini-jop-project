224dae67f262d3d3f336acc7583abb7dc21fe204
Abdu201213 <sabdu9049@gamil.com>
2025-02-15 09:46:37 +0300

    error page complete

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import NotFound from '../assets/images/not-found.svg'
+import '../styles/Error.css'
 function Error() {
   return (
-    <div>Error</div>
+    <div className='error-container'>
+      <img src={NotFound} alt='Not Found' />
+      <div>
+        <h2>OHH! Page Not Found</h2>
+        <p>We can't seem to find the page you are looking for</p>
+        <Link to='/' >Back Home</Link>
+
+      </div>
+    </div>
   )
 }
 
